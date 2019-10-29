6e588647850472df2817f6d86c4fd4af2754e235
Vishal <neharkar.vishal@gmail.com>
2019-10-29 21:52:06 +0530

    Init...🚀

---
+++ README.md
@@ -0,0 +1,9 @@
+## Sample web application
+
+This is a sample web application from a Spring Boot project using Reactive web to handle a request.
+
+The business logic is simulated by reversing a String that comes as an HTTP parameter.
+
+
+Open:
+[http://localhost:8080/hello?name=FullName](http://localhost:8080/hello?name=FullName)
---
+++ src/main/java/com/springreactive/webapp/WebappApplication.java
@@ -0,0 +1,47 @@
+package com.springreactive.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class WebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebappApplication.class, args);
+  }
+
+  @Bean
+  public RouterFunction<ServerResponse> route(GreetingsHandler greetingHandler) {
+    return RouterFunctions
+        .route(RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+            greetingHandler::hello);
+  }
+
+  @Component
+  class GreetingsHandler {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+      String name = request.queryParam("name").get();
+      String reversed = reverse(name);
+      return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+          .body(BodyInserters.fromObject("Hello, " + reversed + "!"));
+    }
+
+    String reverse(String text) {
+      return text.chars()
+          .mapToObj(c -> (char) c)
+          .reduce("", (s, c) -> c + s, (s1, s2) -> s2 + s1);
+    }
+  }
+}
+
